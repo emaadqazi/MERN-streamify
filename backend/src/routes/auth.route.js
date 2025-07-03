6e498fed6d240e13499d38c815348f60d55571c8
Emaad Qazi <qaziemaad@gmail.com>
2025-07-03 07:58:56 -0400

    Added onboarding error trap so we know when user has entered onboarding information that will help recommend friends

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { Signup, Logout, Login } from "../controllers/auth.controller.js"
+import { Signup, Logout, Login, Onboard } from "../controllers/auth.controller.js";
+import { protectRoute } from "../../middleware/auth.middleware.js";
 
 const router = express.Router()
 
 router.post("/signup", Signup);
 router.post("/login", Login);
 router.post("/logout", Logout);
+router.post("/onboarding", protectRoute, Onboard)
 
 export default router;
